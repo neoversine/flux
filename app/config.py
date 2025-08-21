9b9e21f08d42077c17be6d8aa72c56f5c0299f17
Ank <106385729+karak02@users.noreply.github.com>
2025-08-22 00:34:28 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ from pydantic_settings import BaseSettings
 from typing import ClassVar
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "mongodb+srv://neoversine:XhLutMqwAVTWUxzO@imagen.2qoqqc1.mongodb.net/?retryWrites=true&w=majority&appName=imagen"
-    DATABASE_NAME: str = "fastapi_api_db"
-    JWT_SECRET: str = "neoversine1324"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    MONGO_URI: str 
+    DATABASE_NAME: str
+    JWT_SECRET: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int =60
 
     class Config:
         env_file = ".env"
