35bf6c87d4c7872abdcf8062b776025ea3e9e653
Arka probha Roy <100460957+Arkapro1@users.noreply.github.com>
2025-08-30 16:10:04 +0000

    MiddleWare Updated

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app.include_router(usage.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
